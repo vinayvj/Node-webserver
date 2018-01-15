3863ad31e89acf5ce9bf5dd8e9848ae72ccde8d2
vinay <vinay.vjr123@gmail.com>
2018-01-15 08:45:28 -0500

    add project page

--- server.js
+++ server.js
@@ -58,6 +58,12 @@ app.get('/about', (req,res) =>{
 });
 
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pagetitle: 'Projects'
+  })
+})
+
 app.get('/bad', (req,res) => {
   res.send({
     errorMessage: 'Unable to handle request'
